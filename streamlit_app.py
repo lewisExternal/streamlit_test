5286ab046e7c60220735bad4e332d09f5745e70d
Lewis James <lewis.oliver.james@gmail.com>
2025-03-20 22:40:32 +0000

    add requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from streamlit_free_text_select import st_free_text_select
 
 st.write('Hello world!')
+
+value = st_free_text_select(
+    label="Query data:",
+    options=["Option 1", "Option 2"],
+    index=None,
+    format_func=lambda x: x.lower(),
+    placeholder="Select or enter a question",
+    disabled=False,
+    delay=300,
+    label_visibility="visible",
+)
+
+st.write(value)
